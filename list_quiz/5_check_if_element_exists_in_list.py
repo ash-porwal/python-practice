7e3e92587a3b6e1a2adda9487f4d6168f4aa0565
Ashish Porwal <104260822+ash-porwal@users.noreply.github.com>
2024-09-23 15:52:58 +0100

    updated

--- list_quiz/5_check_if_element_exists_in_list.py
+++ list_quiz/5_check_if_element_exists_in_list.py
@@ -10,3 +10,10 @@ n = int(input("Number to test: "))
 for i in some_list:
     if n == i:
         print(True)
+
+# better is if we do like below - 
+if n in some_list:
+    print("Yes")
+
+# or as a one liner
+print("Yes") if n in some_list else "No"
--- list_quiz/7_reversing_a_List.py
+++ list_quiz/7_reversing_a_List.py
@@ -31,3 +31,34 @@ original_list = [10, 11, 12, 13, 14, 15]
 new_list = [original_list[len(original_list) - i]
 			for i in range(1, len(original_list)+1)]
 print(new_list)
+
+
+
+#### Concept -
+# I did this
+l=[5,4]
+# and if we print
+print(l[-1]) # then we get 4 as on output
+
+# but when i do 
+l.insert(-1, 100)
+print(l) # then we get output: [5, 100, 4]
+# but why??
+
+# Explain - 
+"""
+In Python, using the -1 index refers to the position just before the last element.
+
+The key difference here is that negative indexing (like l[-1]) 
+and the behavior of the insert method are two separate mechanisms in Python
+
+- Negative indexing:
+    When you access an element using a negative index, Python counts from the end of the list.
+    l[-1] means "give me the last element."
+    So, l[-1] fetches the value at the last position.
+
+- insert method:
+    The insert(index, value) method places the value before the given index.
+    So when you do l.insert(-1, 100), 
+    Python inserts 100 before the element at index -1, which is the last element.
+"""
